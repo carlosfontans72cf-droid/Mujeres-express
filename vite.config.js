b26b75f87067a3d1dec221bece3dfa46b9320b20
carlosfontans72cf-droid <carlosfontans72.cf@gmail.com>
2026-08-26 12:51:00 -0300

    Arreglo permisos y build

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,6 @@ export default defineConfig({
   root: 'public',
   build: {
     outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        cliente: './pages/cliente.html',
-        comercio: './pages/comercio.html',
-        repartidor: './pages/repartidor.html',
-        dueno: './pages/dueno.html'
-      }
-    }
-  },
-  server: {
-    port: 3000
+    emptyOutDir: true
   }
 });
