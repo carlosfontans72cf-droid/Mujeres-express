03f3851a527d9c8817026137a58ceb18da65df6a
carlosfontans72cf-droid <carlosfontans72.cf@gmail.com>
2026-08-26 14:26:14 -0300

    Corregir storageBucket real

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzNje5bQZ_5P0s0KQeK0s0KQeK0s0KQeK0s0",
-  authDomain: "mujer-express.firebaseapp.com",
-  projectId: "mujer-express",
-  storageBucket: "mujer-express.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abc123def456ghi789jkl01"
+  apiKey: "AIzaSyDCgqnWzMnWJhkdT6xtN5wzTxFxwg9Lla8",
+  authDomain: "mujeres-express.firebaseapp.com",
+  projectId: "mujeres-express",
+  storageBucket: "mujeres-express.firebasestorage.app",
+  messagingSenderId: "836599127765",
+  appId: "1:836599127765:web:b1164243c033ce82080c5c"
 };
 
 const app = initializeApp(firebaseConfig);
